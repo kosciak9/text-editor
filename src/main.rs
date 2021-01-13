adf64283a854189934dd9c81df233f69d0c620cd
Magdalena Lipka <magdalenalipka@int.pl>
2021-01-14 00:23:47 +0100

    cursor moving logic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod editor;
+mod terminal;
 
 use editor::Editor;
+pub use editor::Position;
+pub use terminal::Terminal;
 
 fn main() {
     let editor = Editor::default().run();
